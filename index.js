6d6f1ec4ee79afbd4eebb3cdcf7905a300de07f4
Tomas Smilgius <tsmi@danskebank.lt>
2019-12-02 12:46:45 +0200

    formated result

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const { MongoClient } = require('mongodb');
+const moment = require('moment');
 
 async function main(){
-    const uri = 'mongodb+srv://tsmilgius:<password>@temps-39kmv.azure.mongodb.net/test?retryWrites=true&w=majority';
+    const uri = 'mongodb+srv://tsmilgius:E58-Qegqddf$KdW@temps-39kmv.azure.mongodb.net/test?retryWrites=true&w=majority';
     const client = new MongoClient(uri);
     try {
         await client.connect();
         //await listDatabases(client);
         //await findAllDocuments(client);
         const dataObject = await findMostRecentData(client);
-        console.log(dataObject[0].temperature.toString());
+        console.log(formatResult(dataObject));
+        console.log()
     } catch (e) {
         console.error(e);
     } finally {
@@ -38,6 +40,15 @@ async function findMostRecentData(client) {
     } else {
         console.log('No results');
     }
+}
+
+function formatResult(dataObject) {
+  const temperature = dataObject[0].temperature;
+  const humidity = dataObject[0].humidity;
+  const time = moment(dataObject[0].addedDate).format('YYYY-MM-DD HH:mm');
+
+  const formatedResult = time + ' buvo ' + temperature + '°C' + ' o santykinis oro drėgnumas: ' + humidity + '%';
+  return formatedResult;
 }
 
 main().catch(console.err);
